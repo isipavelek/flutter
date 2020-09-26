fa241736dc91a31171b86dedfa8071b0a51160a9
Isi <ipavelek@gmail.com>
2020-09-26 02:11:40 -0300

    2

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'De fiesta vs code  '),
+      home: MyHomePage(title: 'De fiesta vs code 23  '),
     );
   }
 }
